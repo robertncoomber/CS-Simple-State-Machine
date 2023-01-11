cb1dcf8d00b774fe6e7df624b3552eea13947f57
Robert Coomber <robertnc@gmail.com>
2023-01-10 17:03:40 -0800

    Create TrackState.cs

---
+++ TrackState.cs
@@ -0,0 +1,8 @@
+public class TrackState
+{
+    public string name;
+    protected ArenaTrackStateMachine stateMachine;
+    
+    public virtual void Exit(){}
+    public virtual void Update(){}
+}
