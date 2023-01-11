5654a75d6689913ad2899d7f0a46f81ca8896820
Robert Coomber <robertnc@gmail.com>
2023-01-10 17:04:36 -0800

    Create NotPlayingState.cs

---
+++ NotPlayingState.cs
@@ -0,0 +1,20 @@
+public class NotPlayingState : TrackState
+{
+    public NotPlayingState(ArenaTrackStateMachine state)
+    {
+        stateMachine = state;
+        UnityEngine.Debug.Log("entered not playing state");
+        name = "NotPlayingState";
+    }
+    public override void Exit()
+    {
+        if(stateMachine.currentState.name == "NotPlayingState"){
+            stateMachine.currentState = new IsPlayingState(stateMachine);
+        }
+    }
+
+    public override void Update()
+    {
+        UnityEngine.Debug.Log("currently in not playing state");
+    }
+}
